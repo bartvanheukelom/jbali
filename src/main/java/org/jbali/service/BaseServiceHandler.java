bf0ce5bfeeed6ab62b3e997673da9b0237276328
Bart van Heukelom <mail@bartvh.nl>
2017-11-15 13:03:37 +0100

    generic context parameter for service handler

--- src/main/java/org/jbali/service/BaseServiceHandler.java
+++ src/main/java/org/jbali/service/BaseServiceHandler.java
@@ -1,22 +1,22 @@
 package org.jbali.service;
 
-import java.util.NoSuchElementException;
-
 import com.google.common.collect.ImmutableMap;
+
+import java.util.NoSuchElementException;
 
 /**
  * Abstract base class for generated endpoint-based {@link ServiceHandler}s.
  */
-public abstract class BaseServiceHandler implements ServiceHandler {
+public abstract class BaseServiceHandler<C> implements ServiceHandler<C> {
 
-	private final ImmutableMap<String, OperationHandler> handlers;
+	private final ImmutableMap<String, OperationHandler<C>> handlers;
 	
-	protected BaseServiceHandler(ImmutableMap<String, OperationHandler> handlers) {
+	protected BaseServiceHandler(ImmutableMap<String, OperationHandler<C>> handlers) {
 		this.handlers = handlers;
 	}
 
-	public OperationHandler getOperation(String operation) {
-		final OperationHandler op = handlers.get(operation);
+	public OperationHandler<C> getOperation(String operation) {
+		final OperationHandler<C> op = handlers.get(operation);
 		if (op == null) {
 			throw new NoSuchElementException("Unknown operation " + operation);
 		}
--- src/main/java/org/jbali/service/ServiceHandler.java
+++ src/main/java/org/jbali/service/ServiceHandler.java
@@ -3,12 +3,12 @@ package org.jbali.service;
 /**
  * Take a request in the form of an operation name and input data, and see that it's properly processed.
  */
-public interface ServiceHandler {
+public interface ServiceHandler<C> {
 	
-	public interface OperationHandler {
-		Object handle(Object impl);
+	interface OperationHandler<C> {
+		Object handle(C context, Object input);
 		Class<?> getInputType();
 	}
 
-	OperationHandler getOperation(String operation);
+	OperationHandler<C> getOperation(String operation);
 }
