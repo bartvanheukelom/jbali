fd99b11323a8fd886b94fe4c99278004dd5ba69b
Bart van Heukelom <mail@bartvh.nl>
2018-04-03 14:28:54 +0200

    new Event class

---
+++ src/main/java/org/jbali/util/Event.kt
@@ -0,0 +1,70 @@
+package org.jbali.util
+
+import org.slf4j.Logger
+import java.lang.ref.WeakReference
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+
+class EventDelegate<P> {
+    private val constructed = AtomicReference<Event<P>>()
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Event<P> {
+        val c = constructed.get()
+        return if (c != null) c else {
+            val newc = Event<P>(property)
+            constructed.compareAndSet(null, newc)
+            constructed.get()
+        }
+    }
+}
+
+class Event<P>(
+        val name: String? = null
+){
+
+    constructor(dispatcher: String, prop: KProperty<*>): this("$dispatcher.${prop.name}")
+    constructor(dispatcher: KClass<*>, prop: KProperty<*>): this("${dispatcher.qualifiedName}.${prop.name}")
+    constructor(prop: KProperty<*>): this(prop.toString().removePrefix("val ").removePrefix("var ").replaceAfter(':', "").dropLast(1))
+
+    val listeners: MutableSet<EventListener<P>> = ConcurrentHashMap.newKeySet()
+
+    fun listen(name: String?, callback: (arg: P) -> Unit): EventListener<P> {
+        val l = EventListener(WeakReference(this), name ?: callback.toString(), callback)
+        listeners.add(l)
+        return l
+    }
+    fun listen(callback: (arg: P) -> Unit) = listen(null, callback)
+    operator fun invoke(callback: (arg: P) -> Unit) = listen(null, callback)
+
+    fun dispatch(data: P, errCb: ((l: EventListener<P>, e: Throwable) -> Unit)? = null) {
+        for (l in listeners.toList()) {
+            try {
+                l.callback(data)
+            } catch (e: Throwable) {
+                try {
+                    errCb?.invoke(l, e)
+                } catch (ee: Throwable) {
+                    ee.printStackTrace()
+                }
+            }
+        }
+    }
+    fun dispatch(data: P, errLog: Logger) {
+        dispatch(data, { l, e -> errLog.error("Error in $this listener ${l.name}", e) })
+    }
+
+    override fun toString() = "Event[$name]"
+
+}
+
+class EventListener<P>(
+        val event: WeakReference<Event<P>>,
+        val name: String?,
+        val callback: (arg: P) -> Unit
+) {
+    fun detach() {
+        event.get()?.listeners?.remove(this)
+    }
+    override fun toString() = "Event[$name]"
+}
---
+++ src/test/java/org/jbali/util/EventTest.kt
@@ -0,0 +1,67 @@
+package org.jbali.util
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class EventTest {
+
+    private val evNoName = Event<String>()
+    private val evExpName = Event<String>("HelloWorld")
+    private val evPropName: Event<String> = Event(this::evPropName)
+    private val evPropName2: Event<String> = Event("Foo", this::evPropName2)
+    private val evPropName3: Event<String> = Event(EventTest::class, this::evPropName3)
+    private var evPropName4: Event<String> = Event(this::evPropName)
+    private val evDelegate by EventDelegate<String>()
+
+    @Test fun testEventNames() {
+        assertNull(evNoName.name)
+        assertEquals("HelloWorld", evExpName.name)
+        assertEquals("org.jbali.util.EventTest.evPropName", evPropName.name)
+        assertEquals("Foo.evPropName2", evPropName2.name)
+        assertEquals("org.jbali.util.EventTest.evPropName3", evPropName3.name)
+        assertEquals("org.jbali.util.EventTest.evPropName4", evPropName4.name)
+        assertEquals("org.jbali.util.EventTest.evDelegate", evDelegate.name)
+
+        assertTrue(evDelegate === evDelegate)
+    }
+
+    private val onFoo: Event<String> = Event(this::onFoo)
+
+    fun throwing(x: String): Nothing = throw IllegalStateException(x)
+
+    @Test fun testEvents() {
+
+        println(onFoo)
+
+        var x = ""
+        var y = ""
+        val lstX = onFoo { x = it }
+        onFoo { y = "B$it" }
+
+        val lstThatThrows = onFoo(::throwing)
+        for (i in 0..100) onFoo {}
+
+        assertEquals("AmazingTestListener", onFoo.listen("AmazingTestListener", {}).name)
+
+        var caughtError: Pair<EventListener<String>, Throwable>? = null
+        onFoo.dispatch("hello", { l, e ->
+            println("Error in listener ${l.name}")
+            caughtError = Pair(l, e)
+        })
+
+        assertEquals("hello", x)
+        assertEquals("Bhello", y)
+
+        assertEquals(lstThatThrows, caughtError!!.first)
+        assertTrue(caughtError!!.second is IllegalStateException)
+
+        lstX.detach()
+        onFoo.dispatch("goodbye")
+        assertEquals("hello", x)
+        assertEquals("Bgoodbye", y)
+
+    }
+
+}
