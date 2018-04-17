e4c3180df58cf73b7dd648192321a3ab1b7f7f8b
Bart van Heukelom <mail@bartvh.nl>
2018-04-17 16:14:44 +0200

    Iterable<T>.stream()

--- src/main/java/org/jbali/streams/StreamTools.kt
+++ src/main/java/org/jbali/streams/StreamTools.kt
@@ -1,7 +1,9 @@
 package org.jbali.streams
 
 import java.util.stream.Stream
+import java.util.stream.StreamSupport
 
+fun <T> Iterable<T>.stream(): Stream<T> = StreamSupport.stream(spliterator(), false)
 
 fun <C, V> Stream<V>.collect(supplier: () -> C, adder: C.(inValue: V) -> Unit): C {
     return collectTo(supplier(), adder)
