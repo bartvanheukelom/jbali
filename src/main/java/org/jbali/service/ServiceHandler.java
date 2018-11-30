11b204bf7102ea6e000a78e71c97e1461b4b6bfa
Bart van Heukelom <mail@bartvh.nl>
2018-11-30 16:35:16 +0100

    service op can return null

--- src/main/java/org/jbali/service/ServiceHandler.java
+++ src/main/java/org/jbali/service/ServiceHandler.java
@@ -1,11 +1,14 @@
 package org.jbali.service;
 
+import org.jetbrains.annotations.Nullable;
+
 /**
  * Take a request in the form of an operation name and input data, and see that it's properly processed.
  */
 public interface ServiceHandler<C> {
 	
 	interface OperationHandler<C> {
+		@Nullable
 		Object handle(C context, Object input);
 		Class<?> getInputType();
 	}
