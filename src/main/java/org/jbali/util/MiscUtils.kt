15ee656b6684a8dd42d8b2cde17d546f8191d1dc
Bart van Heukelom <mail@bartvh.nl>
2019-01-14 14:02:00 +0100

    enum jpql prop

--- src/main/java/org/jbali/util/MiscUtils.kt
+++ src/main/java/org/jbali/util/MiscUtils.kt
@@ -43,6 +43,7 @@ fun <A,B> Either<A,B>.any() = fold({it},{it})
  * The fully qualified name of this enum constant, e.g. foo.bar.Color.RED
  */
 val Enum<*>.fullname get() = javaClass.canonicalName + "." + name
+val Enum<*>.jpql get() = "\"${javaClass.canonicalName}.$name\""
 
 // now wouldn't it be nice if Kotlin could just inline this as a constexpr?
 inline fun <reified T : Enum<T>> jpaEnum(e: T) = T::class.qualifiedName + "." + e.name
