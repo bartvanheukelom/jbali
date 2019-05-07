4cfc6e120cc9eb5bcbf13249228f23241451eb53
Bart van Heukelom <mail@bartvh.nl>
2019-05-07 16:39:31 +0200

    json utils

--- src/main/java/org/jbali/kotser/StdSerialization.kt
+++ src/main/java/org/jbali/kotser/StdSerialization.kt
@@ -3,6 +3,7 @@ package org.jbali.kotser
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.modules.SerialModule
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.plus
@@ -20,6 +21,10 @@ object StdJSON {
     // temporary until below is fixed
     fun plainWith(c: SerialModule) = Json(JsonConfiguration(useArrayPolymorphism=true), stdSerializationContext + c)
 }
+
+// TODO find out how this is properly done
+fun Json.stringify(j: JsonElement): String = stringify(JsonElement.serializer(), j)
+fun makeJsonPretty(t: String) = Json.indented.stringify(Json.plain.parseJson(t))
 
 // DOES NOT WORK because context + sm includes jsonModule twice
 //fun Json.withContext(sm: SerialModule) =
