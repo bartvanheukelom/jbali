fe8bbdb2c40d7e3b63d2305e6c2fe8e32d1bef57
Bart van Heukelom <mail@bartvh.nl>
2019-05-07 15:37:30 +0200

    StdJSON.plainWith

--- src/main/java/org/jbali/kotser/StdSerialization.kt
+++ src/main/java/org/jbali/kotser/StdSerialization.kt
@@ -3,7 +3,9 @@ package org.jbali.kotser
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
+import kotlinx.serialization.modules.SerialModule
 import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.plus
 
 val stdSerializationContext = SerializersModule {
     include(dateTimeSerModule)
@@ -14,4 +16,14 @@ val stdSerializationContext = SerializersModule {
 object StdJSON {
     val plain = Json(JsonConfiguration(useArrayPolymorphism=true), stdSerializationContext)
     val indented = Json(JsonConfiguration(useArrayPolymorphism=true, prettyPrint = true), stdSerializationContext)
+
+    // temporary until below is fixed
+    fun plainWith(c: SerialModule) = Json(JsonConfiguration(useArrayPolymorphism=true), stdSerializationContext + c)
 }
+
+// DOES NOT WORK because context + sm includes jsonModule twice
+//fun Json.withContext(sm: SerialModule) =
+//        Json(
+//                Json::class.declaredMemberProperties.single { it.name == "configuration" }.get(this) as JsonConfiguration,
+//                context + sm
+//        )
