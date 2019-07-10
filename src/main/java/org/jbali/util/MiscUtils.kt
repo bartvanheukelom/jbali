320883a527b2da6bfe83abeb0848e12302ff7e64
Bart van Heukelom <mail@bartvh.nl>
2019-07-10 17:15:35 +0200

    ExplainedBool and OutVar

--- src/main/java/org/jbali/util/MiscUtils.kt
+++ src/main/java/org/jbali/util/MiscUtils.kt
@@ -88,3 +88,54 @@ fun <T> Iterable<T>.forEachCatching(
         }
     }
 }
+
+class OutVar<T : Any> {
+    lateinit var value: T
+    override fun toString() = value.toString()
+    override fun equals(other: Any?) = if (other is OutVar<*>) value == other.value else value == other
+    override fun hashCode() = value.hashCode()
+}
+
+data class ExplainedBool @JvmOverloads constructor(
+        val value: Boolean,
+        val explanation: String = "byDefault"
+) {
+    constructor(value: Boolean, explanationRequested: Boolean, explainer: () -> String) :
+            this(
+                    value = value,
+                    explanation = if (explanationRequested) explainer() else "<explanation not requested>"
+            )
+
+    override fun toString() = "${value.toString().padEnd(5)} because $explanation"
+
+    /** Because `if (foo().isTrue())` reads better than `if (foo().value)` */
+    fun isTrue() = value
+    fun isFalse() = !value
+
+    /**
+     * Allows the following usage pattern:
+     * ```
+     * var exp = OutVar<String>()
+     * return
+     *   if (explainedFoo().isFalse(exp)) false because "foo said $exp"
+     *   else (explainedBar().isTrue(exp)) true because "bar said $exp"
+     *   else true because "why not"
+     * }
+     * ```
+     */
+    fun isTrue(outExplanation: OutVar<String>): Boolean {
+        outExplanation.value = explanation
+        return value
+    }
+
+    /**
+     * See isTrue for doc in reverse
+     */
+    fun isFalse(outExplanation: OutVar<String>): Boolean {
+        outExplanation.value = explanation
+        return !value
+    }
+
+}
+
+infix fun Boolean.because(explanation: String) = ExplainedBool(this, explanation)
