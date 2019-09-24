87b9f574da36bdceff3c673b00aa328db3ca86ca
Bart van Heukelom <mail@bartvh.nl>
2019-09-24 18:30:05 +0200

    Bytes4 inline and cleanup

--- src/main/java/org/jbali/bytes/Bytes.kt
+++ src/main/java/org/jbali/bytes/Bytes.kt
@@ -18,8 +18,8 @@ infix fun ByteArray.xor(mask: ByteArray): ByteArray {
 
 // TODO make the value an Int
 inline class Bytes4(val array: ByteArray = ByteArray(4)) {
-    operator fun get(i: Int) = array[i]
-    operator fun set(i: Int, value: Byte) { array[i] = value }
+    inline operator fun get(i: Int): Byte = array[i]
+    inline operator fun set(i: Int, value: Byte) { array[i] = value }
 }
 
 /**
@@ -38,10 +38,6 @@ fun DataOutput.write(b4: Bytes4) = write(b4.array)
 @Suppress("NOTHING_TO_INLINE")
 inline infix fun Bytes4.xor(mask: Bytes4): Bytes4 {
     val res = Bytes4()
-//    res[0] = this[0] xor mask[0]
-//    res[1] = this[1] xor mask[1]
-//    res[2] = this[2] xor mask[2]
-//    res[3] = this[3] xor mask[3]
     repeat4 {
         res[it] = this[it] xor mask[it]
     }
@@ -55,10 +51,6 @@ inline infix fun Bytes4.xor(mask: Bytes4): Bytes4 {
 @Suppress("NOTHING_TO_INLINE")
 inline fun ByteArray.xor4(offset: Int, mask: Bytes4): Bytes4 {
     val res = Bytes4()
-//    res[0] = this[offset+0] xor mask[0]
-//    res[1] = this[offset+1] xor mask[1]
-//    res[2] = this[offset+2] xor mask[2]
-//    res[3] = this[offset+3] xor mask[3]
     repeat4 {
         res[it] = this[offset+it] xor mask[it]
     }
@@ -68,10 +60,6 @@ inline fun ByteArray.xor4(offset: Int, mask: Bytes4): Bytes4 {
 /** XOR4 in place */
 @Suppress("NOTHING_TO_INLINE")
 inline fun ByteArray.xor4ip(offset: Int, mask: Bytes4) {
-//    this[offset+0] = this[offset+0] xor mask.array[0]
-//    this[offset+1] = this[offset+1] xor mask.array[1]
-//    this[offset+2] = this[offset+2] xor mask.array[2]
-//    this[offset+3] = this[offset+3] xor mask.array[3]
     repeat4 {
         this[offset+it] = this[offset+it] xor mask[it]
     }
