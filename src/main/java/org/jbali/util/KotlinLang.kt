59a5ab70ec616449888193bd0b3c4a0b726c7e26
Bart van Heukelom <mail@bartvh.nl>
2020-01-24 01:46:45 +0100

    Kotlin language utils

---
+++ src/main/java/org/jbali/util/KotlinLang.kt
@@ -0,0 +1,56 @@
+package org.jbali.util
+
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Base interface that can be used for implementing property delegate *providers* of read-only properties.
+ *
+ * [https://kotlinlang.org/docs/reference/delegated-properties.html#providing-a-delegate-since-11]
+ *
+ * This is provided only for convenience; you don't have to extend this interface
+ * as long as your property delegate has methods with the same signatures.
+ *
+ * @param R the type of object which owns the delegated property.
+ * @param T the type of the property value.
+ */
+interface ReadOnlyPropertyProvider<in R, out T> {
+    operator fun provideDelegate(thisRef: R, property: KProperty<*>): ReadOnlyProperty<R, T>
+}
+
+/**
+ * Read-only property delegate that returns the given fixed value.
+ */
+data class FixedValueDelegate<T>(val value: T) : ReadOnlyProperty<Any?, T> {
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T = value
+}
+
+/**
+ * Property delegate provider that calls initer with a reference to the property and
+ * provides the property with a delegate that will always return initer's return value.
+ *
+ * ```
+ * val del = InitFixedValueDelegate<String> { prop ->
+ *   System.getProperties().getValue("config.${prop.name}").toString()
+ * }
+ * val config = object {
+ *   val env  by del
+ *   val user by del
+ * }
+ * ```
+ */
+data class InitFixedValueDelegate<T>(val initer: (KProperty<*>) -> T): ReadOnlyPropertyProvider<Any?, T> {
+    override operator fun provideDelegate(thisRef: Any?, property: KProperty<*>): FixedValueDelegate<T> =
+            FixedValueDelegate(try {
+                initer(property)
+            } catch (e: Throwable) {
+                throw RuntimeException("Could not init property $property of $thisRef: $e", e)
+            })
+}
+
+data class PrintingFixedValueDelegate<T>(val value: T): ReadOnlyPropertyProvider<Any?, T> {
+    override fun provideDelegate(thisRef: Any?, property: KProperty<*>): ReadOnlyProperty<Any?, T> {
+        println("${property.name} = $value")
+        return FixedValueDelegate(value)
+    }
+}
