d67345281627f5ba93eed54b7a9df4575bc01130
Bart van Heukelom <mail@bartvh.nl>
2020-02-11 17:44:57 +0100

    objmap to own file and beginning support for mutablemap

---
+++ src/main/java/org/jbali/util/ObjMap.kt
@@ -0,0 +1,163 @@
+package org.jbali.util
+
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty1
+import kotlin.reflect.KVisibility
+import kotlin.reflect.full.memberProperties
+
+/**
+ * Present the public member properties of any object as a Map.
+ * Doesn't copy anything, it's just a facade over the given object,
+ * so it inherits mutability and thread-safety from that object.
+ */
+class ObjMap<T : Any>(
+        private val clazz: ClassMapInfo<T>,
+        val obj: T
+) : Map<String, Any?> { // TODO MutableMap
+
+    @Suppress("UNCHECKED_CAST")
+    constructor(obj: T): this(
+            obj.javaClass.kotlin.classMapInfo,
+            obj)
+
+    override val keys = clazz.keys
+    override val size = clazz.keys.size
+
+    // TODO only on get, return implementation that already has templates in ClassMapInfo
+    override val entries: Set<Map.Entry<String, Any?>> =
+            clazz.props.map {
+                object : Map.Entry<String, Any?> {
+                    override val key = it.key
+                    override val value = it.value.get(obj)
+
+                    // TODO the following is the same for all implementations of Map.Entry, should be centralized somewhere (probably already in a lib)
+
+                    override fun equals(other: Any?) =
+                            other is Map.Entry<*, *> &&
+                                    key == other.key &&
+                                    value == other.value
+
+                    override fun hashCode(): Int =
+                            key.hashCode() xor value.hashCode()
+
+                    override fun toString(): String =
+                            "$key=$value"
+                }
+            }.toSet()
+
+    override val values =
+            clazz.props.map {
+                it.value.get(obj)
+            }
+
+    override fun containsKey(key: String) = clazz.props.containsKey(key)
+
+    override fun containsValue(value: Any?) =
+            clazz.props.values.any {
+                it.get(obj) == value
+            }
+
+    override fun get(key: String) = clazz.props[key]?.get(obj)
+    override fun isEmpty() = clazz.props.isEmpty()
+
+    override fun toString() =
+            StringBuilder(size * 128).apply {
+                append('{')
+                var first = true
+                entries.forEach { e ->
+                    if (first) {
+                        first = false
+                    } else {
+                        append(", ")
+                    }
+                    append(e.key)
+                    append('=')
+                    append(e.value)
+                }
+                append('}')
+            }.toString()
+
+    override fun equals(other: Any?) =
+            when (other) {
+
+                this ->
+                    true
+
+                is ObjMap<*> ->
+                    other.obj == obj
+
+                is Map<*, *> ->
+                    other.size == size &&
+                            toMap() == other // TODO optimize?
+
+                else ->
+                    false
+
+            }
+
+    override fun hashCode() =
+            entries.sumBy { it.hashCode() }
+
+}
+
+/**
+ * [ObjMap] wrapper for the receiver.
+ */
+val <T : Any> T.asMap: Map<String, Any?> get() = ObjMap(this)
+
+
+/**
+ * Caches the per-class information used by [ObjMap].
+ */
+data class ClassMapInfo<T : Any>(val clazz: KClass<T>) {
+
+    val props: Map<String, KProperty1<T, *>> =
+            clazz.memberProperties
+                .filter { it.visibility == KVisibility.PUBLIC }
+                .associateBy { it.name }
+
+    // implements MutableSet so ObjMap can implement MutableMap,
+    // but is of course not actually mutable.
+    val keys: MutableSet<String> =
+            object : MutableSet<String>, Set<String> by props.keys {
+
+                override fun iterator() =
+                        object : MutableIterator<String>, Iterator<String> by props.keys.iterator() {
+                            override fun remove() {
+                                unsupported()
+                            }
+                        }
+
+                override fun add(element: String): Boolean {
+                    throw unsupported()
+                }
+
+                override fun addAll(elements: Collection<String>): Boolean {
+                    throw unsupported()
+                }
+
+                override fun clear() {
+                    throw unsupported()
+                }
+
+                override fun remove(element: String): Boolean {
+                    throw unsupported()
+                }
+
+                override fun removeAll(elements: Collection<String>): Boolean {
+                    throw unsupported()
+                }
+
+                override fun retainAll(elements: Collection<String>): Boolean {
+                    throw unsupported()
+                }
+
+            }
+
+    private fun unsupported() =
+            UnsupportedOperationException("ObjMap doesn't support add/remove operations")
+
+}
+
+val <T : Any> KClass<T>.classMapInfo: ClassMapInfo<T>
+        by StoredExtensionProperty(::ClassMapInfo)
--- src/main/java/org/jbali/util/StringMap.kt
+++ src/main/java/org/jbali/util/StringMap.kt
@@ -1,14 +1,12 @@
 package org.jbali.util
 
 import kotlinx.serialization.Serializable
-import kotlin.reflect.KClass
-import kotlin.reflect.KVisibility
 import kotlin.reflect.full.declaredMemberProperties
-import kotlin.reflect.full.memberProperties
 
 
 /**
  * A map wrapper with string keys, which it knows at runtime too.
+ * TODO make interface and let ObjMap implement it too
  */
 @Serializable
 class StringMap<V>(val contents: Map<String, V>) : Map<String, V> by contents {
@@ -27,6 +25,8 @@ class StringMap<V>(val contents: Map<String, V>) : Map<String, V> by contents {
  *     })
  * })
  * Will shallowly copy the data in the object to a new map.
+ *
+ * TODO deprecate because ObjMap is better
  */
 fun objToMap(obj: Any) =
         StringMap(
@@ -34,59 +34,3 @@ fun objToMap(obj: Any) =
                     it.name to it.get(obj)
                 }.toMap()
         )
-
-
-data class ClassMapInfo<T : Any>(val clazz: KClass<T>) {
-    val props = clazz.memberProperties
-            .filter { it.visibility == KVisibility.PUBLIC }
-            .associateBy { it.name }
-    val keys = props.keys
-}
-
-val <T : Any> KClass<T>.classMapInfo: ClassMapInfo<T> by StoredExtensionProperty(::ClassMapInfo)
-
-/**
- * Present the public member properties of any object as a Map. Uses the given object for storage, so take
- * care if it is mutable.
- * TODO move to jbali
- */
-class ObjMap<T : Any>(
-        private val ci: ClassMapInfo<T>,
-        val obj: T
-) : Map<String, Any?> {
-    @Suppress("UNCHECKED_CAST")
-    constructor(obj: T): this(
-            obj.javaClass.kotlin.classMapInfo,
-            obj)
-
-    override val keys = ci.keys
-    override val size = ci.keys.size
-
-    override val entries: Set<Map.Entry<String, Any?>> =
-            ci.props.map {
-                object : Map.Entry<String, Any?> {
-                    override val key = it.key
-                    override val value = it.value.get(obj)
-                }
-            }.toSet()
-
-    override val values =
-            ci.props.map {
-                it.value.get(obj)
-            }
-
-    override fun containsKey(key: String) = ci.props.containsKey(key)
-
-    override fun containsValue(value: Any?) =
-            ci.props.values.any {
-                it.get(obj) == value
-            }
-
-    override fun get(key: String) = ci.props[key]?.get(obj)
-    override fun isEmpty() = ci.props.isEmpty()
-
-    // simplest correct implementation. inefficient, but who uses this anyway?
-    override fun equals(other: Any?) = other is Map<*, *> && toMap() == other
-    override fun hashCode() = toMap().hashCode()
-
-}
