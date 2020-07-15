cdbc611818e70fe70a5d52525df328b3f5164d37
Bart van Heukelom <mail@bartvh.nl>
2020-07-15 13:44:17 +0200

    uncertain doc

--- src/main/java/org/jbali/arrow/Uncertain.kt
+++ src/main/java/org/jbali/arrow/Uncertain.kt
@@ -10,11 +10,19 @@ inline class ErrorMessage(val msg: String)
 /**
  * Represents the result of a call that may give
  * an error message instead of a return value.
+ *
+ * The actual type is [Either],
+ * where a [Left] is called the [errorMessage],
+ * and a [Right] is called the [result].
+ *
+ * Utility functions related to this usage are:
+ * - [getOrThrow]
+ * - [nullToError]
  */
 typealias Uncertain<T> = Either<ErrorMessage, T>
 
 /**
- * Returns the certain value, or throws the error.
+ * Returns the result, or throws the error.
  * @throws IllegalStateException with the [ErrorMessage] as message.
  */
 fun <T> Uncertain<T>.getOrThrow(): T =
@@ -27,6 +35,12 @@ fun <T> Uncertain<T>.getOrThrow(): T =
  */
 fun <T> T?.nullToError(errMsg: () -> String): Either<ErrorMessage, T> =
         when (this) {
-            null -> Left(ErrorMessage(errMsg()))
-            else -> Right(this)
+            null -> errorMessage(errMsg())
+            else -> result(this)
         }
+
+fun <T> result(r: T) =
+        Right(r)
+
+fun errorMessage(msg: String) =
+        Left(ErrorMessage(msg))
