408082d180522564a9a130a2cab8d311d6cad37d
Bart van Heukelom <mail@bartvh.nl>
2020-08-13 17:14:51 +0200

    jar task provider

--- buildSrc/src/main/kotlin/KotlinProject.kt
+++ buildSrc/src/main/kotlin/KotlinProject.kt
@@ -1,15 +1,21 @@
 package org.jbali.gradle
 
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
 import org.gradle.api.artifacts.ConfigurationContainer
 import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.tasks.TaskProvider
+import org.gradle.jvm.tasks.Jar
 
 interface KotlinProject : Project {
 
     val nativeConfigurations: ConfigurationContainer
 
     override fun getConfigurations() = KotlinConfigurationContainer(nativeConfigurations)
+
+    @Suppress("UNCHECKED_CAST")
+    val jar: TaskProvider<Jar> get() = tasks.named("jar") as TaskProvider<Jar>
 
     val DependencyHandler.implementation: Configuration get() = configurations.implementation
     val DependencyHandler.runtimeOnly: Configuration get() = configurations.runtimeOnly
@@ -21,14 +27,16 @@ interface KotlinProject : Project {
 
 open class KotlinProjectWrapper(native: Project) : ProjectWrapper(native), KotlinProject {
     override val nativeConfigurations get() = native.configurations
-    override fun getConfigurations() = super<KotlinProject>.getConfigurations()
+    override fun getConfigurations(): KotlinConfigurationContainer = super<KotlinProject>.getConfigurations()
 }
 
 class KotlinConfigurationContainer(
         private val c: ConfigurationContainer
 ) : ConfigurationContainer by c {
+    // TODO use named() for configuration avoidance
     val implementation get() = getByName("implementation")
     val runtimeOnly get() = getByName("runtimeOnly")
+    val runtimeClasspath get() = getByName("runtimeClasspath")
     val compileOnly get() = getByName("compileOnly")
     val testRuntimeOnly get() = getByName("testRuntimeOnly")
     val testImplementation get() = getByName("testImplementation")
