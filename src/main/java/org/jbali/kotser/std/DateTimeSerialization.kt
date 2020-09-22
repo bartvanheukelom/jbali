e557e327fa043beda5cacff1301f7e666ac9de8c
Bart van Heukelom <mail@bartvh.nl>
2020-09-22 15:04:14 +0200

    serializers for yw ym yq y

--- src/main/java/org/jbali/kotser/std/DateTimeSerialization.kt
+++ src/main/java/org/jbali/kotser/std/DateTimeSerialization.kt
@@ -1,15 +1,17 @@
 package org.jbali.kotser.std
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
 import kotlinx.serialization.modules.SerializersModule
 import org.jbali.kotser.StringBasedSerializer
+import org.jbali.kotser.Transformer
 import org.jbali.kotser.TransformingSerializer
+import org.jbali.kotser.transformingSerializer
 import org.jbali.threeten.toDate
+import org.threeten.extra.YearQuarter
+import org.threeten.extra.YearWeek
 import java.sql.Timestamp
-import java.time.Duration
-import java.time.Instant
-import java.time.LocalDate
-import java.time.ZonedDateTime
+import java.time.*
 import java.util.*
 
 @Serializer(forClass = Instant::class)
@@ -27,6 +29,28 @@ object DateSerializer : TransformingSerializer<Date, Instant>(Date::class, Insta
 object LocalDateSerializer : StringBasedSerializer<LocalDate>(LocalDate::class) {
     override fun fromString(s: String): LocalDate = LocalDate.parse(s)
 }
+
+@Serializer(forClass = YearWeek::class)
+object YearWeekSerializer : StringBasedSerializer<YearWeek>(YearWeek::class) {
+    override fun fromString(s: String): YearWeek = YearWeek.parse(s)
+}
+
+@Serializer(forClass = YearMonth::class)
+object YearMonthSerializer : StringBasedSerializer<YearMonth>(YearMonth::class) {
+    override fun fromString(s: String): YearMonth = YearMonth.parse(s)
+}
+
+@Serializer(forClass = YearQuarter::class)
+object YearQuarterSerializer : StringBasedSerializer<YearQuarter>(YearQuarter::class) {
+    override fun fromString(s: String): YearQuarter = YearQuarter.parse(s)
+}
+
+object YearSerializer : KSerializer<Year> by transformingSerializer(
+        transformer = object : Transformer<Year, Int> {
+            override fun transform(obj: Year) = obj.value
+            override fun detransform(tf: Int): Year = Year.of(tf)
+        }
+)
 
 @Serializer(forClass = ZonedDateTime::class)
 object ZonedDateTimeSerializer : StringBasedSerializer<ZonedDateTime>(ZonedDateTime::class) {
