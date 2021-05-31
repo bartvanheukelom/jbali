499c5e4859a3211e35368eb5eef49cdab631bdc6
Bart van Heukelom <mail@bartvh.nl>
2021-05-31 15:44:00 +0200

    fake exposed transactionmanager to use existing transaction

---
+++ src/jvmMain/java/org/jbali/exposed/externalTransaction.kt
@@ -0,0 +1,83 @@
+package org.jbali.exposed
+
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Transaction
+import org.jetbrains.exposed.sql.statements.jdbc.JdbcConnectionImpl
+import org.jetbrains.exposed.sql.transactions.TransactionInterface
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import java.sql.Connection
+
+fun <T> inTransactionOf(
+    connection: Connection,
+    ignoreCommit: Boolean = false,
+    statement: Transaction.() -> T
+): T {
+    val before = TransactionManager.manager
+    TransactionManager.resetCurrent(ExistingConTxManager(
+        con = connection,
+        ignoreCommit = ignoreCommit,
+    ))
+    try {
+        return TransactionManager.current().statement()
+    } finally {
+        TransactionManager.resetCurrent(before)
+    }
+}
+
+internal class ExistingConTxManager(
+    con: Connection,
+    ignoreCommit: Boolean,
+) : TransactionManager {
+    
+    private val trans =
+        Transaction(object : TransactionInterface {
+            
+            override val connection = JdbcConnectionImpl(con)
+            
+            override val db: Database =
+                Database.connect(
+                    getNewConnection = { throw unsup() },
+                    manager = { this@ExistingConTxManager },
+                )
+            
+            override val outerTransaction: Transaction?
+                get() = null
+            override val transactionIsolation: Int
+                get() = con.transactionIsolation
+            
+            override fun close() {
+                throw unsup()
+            }
+            
+            override fun commit() {
+                if (!ignoreCommit) {
+                    throw unsup()
+                }
+            }
+            
+            override fun rollback() {
+                throw unsup()
+            }
+            
+        })
+    
+    override var defaultIsolationLevel: Int
+        get() = throw unsup()
+        set(value) { throw unsup() }
+    override var defaultRepetitionAttempts: Int
+        get() = throw unsup()
+        set(value) { throw unsup() }
+    
+    override fun bindTransactionToThread(transaction: Transaction?) {
+        throw unsup()
+    }
+    
+    override fun currentOrNull() =
+        trans
+    
+    override fun newTransaction(isolation: Int, outerTransaction: Transaction?) =
+        throw unsup()
+    
+    private fun unsup() = UnsupportedOperationException("inTransactionOf does not support this operation")
+    
+}
