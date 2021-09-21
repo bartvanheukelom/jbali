461f342aaaca675412caf41efe466f38e4495f05
Bart van Heukelom <mail@bartvh.nl>
2021-09-22 01:05:00 +0200

    IdTable extensions and implementations

--- src/jvmMain/java/org/jbali/exposed/idTableExt.kt
+++ src/jvmMain/java/org/jbali/exposed/idTableExt.kt
@@ -1,8 +1,11 @@
 package org.jbali.exposed
 
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IdTable
+import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Query
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.select
 
 fun <T : Comparable<T>> IdTable<T>.select(id: T): Query =
@@ -16,6 +19,13 @@ fun <T : Comparable<T>> IdTable<T>.selectSingle(id: T): ResultRow =
 fun <T : Comparable<T>> IdTable<T>.selectSingleOrNull(id: T): ResultRow? =
     select(id).singleOrNull()
 
+fun <T : Comparable<T>> IdTable<T>.deleteSingle(id: T): Boolean =
+    let { tbl ->
+        deleteWhere {
+            tbl.id eq id
+        } > 0
+    }
+
 
 //@JvmName("setWithEntityIdValue")
 //operator fun <S : Comparable<S>, ID : EntityID<S>, E : S?>
@@ -25,3 +35,8 @@ fun <T : Comparable<T>> IdTable<T>.selectSingleOrNull(id: T): ResultRow? =
 //    require(!values.containsKey(column)) { "$column is already initialized" }
 //    values[column] = value
 //}
+
+open class VarcharIdTable(name: String = "", columnName: String = "id", length: Int = 255) : IdTable<String>(name) {
+    override val id: Column<EntityID<String>> = varchar(columnName, length).entityId()
+    override val primaryKey by lazy { super.primaryKey ?: PrimaryKey(id) }
+}
