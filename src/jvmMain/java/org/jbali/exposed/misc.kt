39a273d63cf9a3f12f812f12140b1f458a17e71e
Bart van Heukelom <mail@bartvh.nl>
2021-12-27 19:13:12 +0100

    SortOrder.exposed

--- src/jvmMain/java/org/jbali/exposed/misc.kt
+++ src/jvmMain/java/org/jbali/exposed/misc.kt
@@ -1,5 +1,6 @@
 package org.jbali.exposed
 
+import org.jbali.util.SortOrder
 import org.jetbrains.exposed.sql.LongColumnType
 import org.jetbrains.exposed.sql.Op
 import org.jetbrains.exposed.sql.QueryBuilder
@@ -22,3 +23,8 @@ fun String.toSqlLiteral(): String =
 
 fun compoundAnd(vararg ops: Op<Boolean>): Op<Boolean> =
     ops.toList().compoundAnd()
+
+val SortOrder.exposed get() = when (this) {
+    SortOrder.ASCENDING  -> org.jetbrains.exposed.sql.SortOrder.ASC
+    SortOrder.DESCENDING -> org.jetbrains.exposed.sql.SortOrder.DESC
+}
