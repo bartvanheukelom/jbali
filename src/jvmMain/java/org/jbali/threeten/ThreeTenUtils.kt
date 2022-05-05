0675b93da0008bf0ab59fde75d7ee1fae9f79252
Bart van Heukelom <mail@bartvh.nl>
2022-05-05 17:31:11 +0200

    added instantMaxYyyy, instantFarFuture, instantForeseeableFuture and instantMaxUnix

--- src/jvmMain/java/org/jbali/threeten/ThreeTenUtils.kt
+++ src/jvmMain/java/org/jbali/threeten/ThreeTenUtils.kt
@@ -19,6 +19,37 @@ val instantMaxDate: Instant =
 val instantVeryMaxDate: Instant =
         Instant.ofEpochMilli(Long.MAX_VALUE).plusNanos(999_999L)
 
+/**
+ * The last instant in UTC whose year can be represented using 4 digits,
+ * i.e. `9999-12-32T23:59:59.999999Z`.
+ */
+val instantMaxYyyy: Instant =
+        Instant.parse("9999-12-31T23:59:59.999999Z")
+
+/**
+ * Instant that is so far in the future that it might as well be infinitely so,
+ * but is still small enough to be representable in many formats, like databases.
+ *
+ * Value: `2999-12-32T23:59:59.999999Z`
+ */
+val instantFarFuture: Instant =
+        Instant.parse("2999-12-31T23:59:59.999999Z")
+
+/**
+ * Instant that is so far in the future that it can practically be used, for now, as far away,
+ * but is still small enough to be representable in most formats, including as 32-bits signed UNIX time.
+ *
+ * Equals [instantMaxUnix].
+ */
+val instantForeseeableFuture get() = instantMaxUnix
+
+/**
+ * The last instant that be represented as 32-bits signed UNIX time,
+ * i.e. `2038-01-19T03:14:07Z`.
+ */
+val instantMaxUnix: Instant =
+        Instant.ofEpochMilli(Int.MAX_VALUE * 1000L)
+
 // TODO toDateExact
 fun Instant.toDate(): Date =
         try {
