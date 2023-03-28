c5eadb0d52e34ab4e98c7dc45cd9c0ebb4fb442e
Bart van Heukelom <mail@bartvh.nl>
2023-03-28 14:16:10 +0200

    isSortedInOrderBy

--- src/jvmMain/java/org/jbali/util/SortOrder.kt
+++ src/jvmMain/java/org/jbali/util/SortOrder.kt
@@ -17,12 +17,14 @@ enum class SortOrder(
     /**
      * Given two values, returns the one that would be listed first in a hypothetical
      * list of [T] naturally sorted using this [SortOrder].
+     *
+     * If values are equal, returns the first one.
      */
     fun <T : Comparable<T>> firstOf(a: T, b: T) =
-            when (this) {
-                ASCENDING -> minOf(a, b)
-                DESCENDING -> maxOf(a, b)
-            }
+        when (this) {
+            ASCENDING -> minOf(a, b)
+            DESCENDING -> maxOf(a, b)
+        }
 }
 
 /**
@@ -32,3 +34,28 @@ enum class SortOrder(
  */
 fun <T, K : Comparable<K>> Iterable<T>.sortedInOrderBy(order: SortOrder, selector: (T) -> K): List<T> =
     sortedWith(compareBy(order.comparator(), selector))
+
+/**
+ * Whether this [Iterable] is sorted according to the given natural [order].
+ *
+ * If the iterable is empty or has only one element, returns true. Consecutive elements
+ * that compare equal are considered sorted.
+ */
+fun <T, K : Comparable<K>> Iterable<T>.isSortedInOrderBy(
+    order: SortOrder = SortOrder.ASCENDING,
+    selector: (T) -> K, // = { it } <- gives type mismatch so added overload below
+): Boolean {
+    val it = iterator()
+    if (!it.hasNext()) return true
+    var prev = selector(it.next())
+    val comp = order.comparator<K>()
+    while (it.hasNext()) {
+        val next = selector(it.next())
+        if (comp.compare(prev, next) > 0) return false
+        prev = next
+    }
+    return true
+}
+fun <T : Comparable<T>> Iterable<T>.isSortedInOrderBy(
+    order: SortOrder = SortOrder.ASCENDING,
+): Boolean = isSortedInOrderBy(order) { it }
