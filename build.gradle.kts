862cb20d203db673cb14eeb3b755ad3b858d903f
Bart van Heukelom <mail@bartvh.nl>
2023-09-14 18:33:06 +0200

    kotlin 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ initKotlinProject(
     group = JBali.group,
     name = JBali.aJbali,
     acceptableKotlinVersions = setOf(
-        KotlinVersions.V1_9_0,
+        KotlinVersions.V1_9_10,
     )
 )
 
