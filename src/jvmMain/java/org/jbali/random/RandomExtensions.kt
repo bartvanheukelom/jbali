352fc70e40e0ad4f51a784c06df5d4cec1f6d8db
Bart van Heukelom <mail@bartvh.nl>
2023-11-22 23:39:35 +0100

    Add Random.nextUUID() in RandomExtensions
    Added a new method, `Random.nextUUID()`, to generate a UUID with version 4 and variant 2. This provides a more flexible way to generate UUID compare to `UUID.randomUUID()` as it allows choosing the random source.

--- src/jvmMain/java/org/jbali/random/RandomExtensions.kt
+++ src/jvmMain/java/org/jbali/random/RandomExtensions.kt
@@ -2,6 +2,10 @@ package org.jbali.random
 
 import org.jbali.bytes.asData
 import org.jbali.math.powerOf10
+import java.nio.ByteBuffer
+import java.util.*
+import kotlin.experimental.and
+import kotlin.experimental.or
 import kotlin.random.Random
 
 /**
@@ -24,4 +28,17 @@ fun Random.nextHex(chars: UInt): String {
     require(chars % 2u == 0u)
     val bytes = chars / 2u
     return nextBytes(bytes.toInt()).asData().toHexString()
+}
+
+/**
+ * Generate a UUID with the version 4 (random) and variant 0b10x (RFC 4122).
+ * This is what [UUID.randomUUID] does, but that method does not allow choosing the random source.
+ */
+fun Random.nextUUID(): UUID {
+    val bytes = ByteBuffer.wrap(nextBytes(16))
+    bytes.put(6, bytes.get(6) and 0b0000_1111)           // bits[48..51] = 0b0000
+    bytes.put(6, bytes.get(6) or  0b0100_0000)           // bits[48..51] = 0b0100 = version 4
+    bytes.put(8, bytes.get(8) and 0b0011_1111)           // bits[64..65] = 0b00
+    bytes.put(8, bytes.get(8) or  0b1000_0000u.toByte()) // bits[64..65] = 0b10 = variant 2
+    return UUID(bytes.long, bytes.long)
 }
