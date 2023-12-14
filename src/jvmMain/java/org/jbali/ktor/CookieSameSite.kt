5f624a07723d01311e03736b5d25d5d25c6b5ac8
Bart van Heukelom <mail@bartvh.nl>
2023-12-14 19:33:55 +0100

    Cookie SameSite=None deprecation note

--- src/jvmMain/java/org/jbali/ktor/CookieSameSite.kt
+++ src/jvmMain/java/org/jbali/ktor/CookieSameSite.kt
@@ -1,6 +1,7 @@
 package org.jbali.ktor
 
-import io.ktor.sessions.CookieConfiguration
+import io.ktor.sessions.*
+import kotlin.collections.set
 
 /**
  * The SameSite attribute lets servers require that a cookie shouldn't be sent with
@@ -11,6 +12,7 @@ enum class CookieSameSite {
 
     /**
      * The cookie is sent only to the same site as the one that originated it.
+     * A request to site B initiated by clicking a link on site A will NOT include B's cookie.
      */
     Strict,
 
@@ -18,12 +20,15 @@ enum class CookieSameSite {
      * The cookie is sent only to the same site as the one that originated it,
      * with an exception for when the user navigates to a URL from an external site,
      * such as by following a link.
+     *
+     * This is the default value in modern browsers and has always been the default behaviour of cookies.
      */
     Lax,
 
     /**
      * The cookie has no restrictions on cross-site requests.
      */
+    @Deprecated("Google Chrome will be phasing out support starting in 2024. Other browsers will likely do so too.")
     None;
 
     companion object {
