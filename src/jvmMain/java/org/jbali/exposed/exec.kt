1d57eb8f33f4dbceaab8c176db3d0c95f36e3181
Bart van Heukelom <mail@bartvh.nl>
2024-11-18 16:40:40 +0100

    exposed Transaction.execAndMap

--- src/jvmMain/java/org/jbali/exposed/exec.kt
+++ src/jvmMain/java/org/jbali/exposed/exec.kt
@@ -1,10 +1,13 @@
 package org.jbali.exposed
 
 import org.intellij.lang.annotations.Language
+import org.jbali.jdbc.map
+import org.jetbrains.exposed.sql.IColumnType
 import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.statements.Statement
 import org.jetbrains.exposed.sql.statements.StatementType
 import org.jetbrains.exposed.sql.statements.api.PreparedStatementApi
+import java.sql.ResultSet
 
 /**
  * Executes the given SQL statement, which must be an SQL Data Manipulation Language (DML) statement, such as INSERT, UPDATE or DELETE; or an SQL statement that returns nothing, such as a DDL statement.
@@ -25,3 +28,17 @@ fun Transaction.execUpdate(
  */
 fun statementTypeOf(@Language("sql") stmt: String): StatementType =
     StatementType.values().single { stmt.trim().startsWith(it.name, true) }
+
+/**
+ * Execute the given query, which should be a SELECT or some other statement that returns a result set,
+ * and map the result rows to a list of [T].
+ */
+fun <T> Transaction.execAndMap(
+    @Language("sql") stmt: String,
+    args: Iterable<Pair<IColumnType, Any?>> = emptyList(),
+    explicitStatementType: StatementType? = null,
+    transform: (ResultSet) -> T
+): List<T> =
+    exec(stmt, args, explicitStatementType) { rs ->
+        rs.map(transform)
+    } ?: error("No result set")
