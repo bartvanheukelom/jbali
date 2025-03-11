9aae7871dc3cf3a2ca7906719db2cfde6b4f7c95
Bart van Heukelom <mail@bartvh.nl>
2025-03-11 13:54:59 +0100

    DynamicMultiRateLimiter

---
+++ src/jvmMain/java/org/jbali/security/DynamicMultiRateLimiter.kt
@@ -0,0 +1,37 @@
+package org.jbali.security
+
+import org.jbali.events.Observable
+import java.time.Instant
+
+class DynamicMultiRateLimiter<O>(
+    private val rules: Observable<List<MultiRateLimiter.Rule<O>>>,
+    private val clock: () -> Instant = { Instant.now() }
+) : OpRateLimiter<O>, AutoCloseable {
+    
+    @Volatile
+    private var limiter = MultiRateLimiter<O>(emptyList(), clock)
+    
+    private val listener = rules.bind {
+        synchronized(this) {
+            val grants = limiter.grantHistory()
+            limiter = MultiRateLimiter(it, clock)
+            limiter.addGrantHistory(grants)
+        }
+    }
+    
+    override fun close() {
+        listener.detach()
+    }
+    
+    override fun getAvailablePermits(op: O) = synchronized(this) {
+        limiter.getAvailablePermits(op)
+    }
+    
+    override fun requestPermits(op: O, permits: UInt, partial: Boolean) = synchronized(this) {
+        limiter.requestPermits(op, permits, partial)
+    }
+    
+    override suspend fun waitForPermits(op: O, permits: UInt, onWait: () -> Unit) =
+        synchronized(this) { limiter }.waitForPermits(op, permits, onWait)
+    
+}
